d04e88846368d2547d0e08c033cc4a86176d3a5a
vlad-duhota <vladarduino@gmail.com>
2022-12-26 12:57:52 +0200

    key sectors && team member

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,28 @@
-import * as flsFunctions from "./modules/functions.js";
 
-flsFunctions.isWebp();
+// import Swiper bundle with all modules installed
+import Swiper from 'swiper/bundle';
 
 // smooth scroll
 document.querySelectorAll('a.anchor').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
-
+        
         document.querySelector(this.getAttribute('href')).scrollIntoView({
             behavior: 'smooth'
         });
     });
 });
+
+// team member swiper
+let swiper = new Swiper('.team-member__swiper', {
+    autoHeight: true,
+    loop: true,
+    pagination: {
+      el: '.team-member__pagination',
+      clickable: true,
+    },
+    navigation: {
+        nextEl: '.team-member__next',
+        // prevEl: '.team-member__prev',
+      },
+  });
